32694af9ba8c08643eadb9d1a8c0c827f16facac
John Doe <johndoe@example.com>
2023-05-10 00:33:24 +0300

    add new samples and 2 filters

--- main.py
+++ main.py
@@ -34,9 +34,38 @@ def contains_speech(src):
     return max(fourier) > 1e7
 
 
-# функция, очищающая высокочастотный и никочастотный шум
+# функция, выполняющая очистку от шума высоких и низких частот
+def bandpass_filter(src):
+    if ".mp3" in src:
+        src = from_mp3_to_wav(src)
+
+    rate, data = scipy.io.wavfile.read(src)
+    channels_cnt = 1
+    window_size = rate
+    data_size = len(data)
+    try:
+        channels_cnt = data.shape[1]
+    except:
+        pass
+
+    for channel in range(channels_cnt):
+        pos = 0
+        while pos + window_size < data_size:
+            transformed_audio = np.fft.fft(data[:, channel][pos: pos + window_size])
+            size = transformed_audio.size
+            transformed_audio[:200] = 0
+            transformed_audio[-200:] = 0
+            transformed_audio[7000:size // 2] = 0
+            transformed_audio[size // 2:-7000] = 0
+            data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
+            pos += window_size
+
+    scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
+
+
+# функция, хорошо очищающая высокочастотный и никочастотный шум
 # также она выполняет сглаживание остального диапазона
-def bad_frequency_cleaner(src):
+def exponentional_filter(src):
     if ".mp3" in src:
         src = from_mp3_to_wav(src)
 
@@ -59,14 +88,39 @@ def bad_frequency_cleaner(src):
             transformed_audio = np.fft.fft(data[:, channel][pos: pos + n])
             inds = np.arange(n)
             alpha = 0.0000001
-            filter = np.exp(-1.5 * alpha * (np.minimum(inds, n - inds) ** 2) / 2)[:n]
+            filter = np.exp(-1.5 * alpha * (np.minimum(inds, n - inds) ** 2) / 2)
             transformed_audio *= filter
 
-            data[:, channel][pos: min(len(data), pos + n)] = np.fft.ifft(transformed_audio).real
+            data[:, channel][pos: pos + n] = np.fft.ifft(transformed_audio).real
             pos += window_size
 
     scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
-    return
 
 
-bad_frequency_cleaner("audio_files/test_audio/RockAndRoll.wav")
+# функция, очищающая периодический шум
+# по первой 0.1 секунду записи
+def spectral_subtraction_filter(src):
+    if ".mp3" in src:
+        src = from_mp3_to_wav(src)
+
+    rate, data = scipy.io.wavfile.read(src)
+    channels_cnt = 1
+    window_size = int(rate * 0.1)
+    data_size = len(data)
+    try:
+        channels_cnt = data.shape[1]
+    except:
+        pass
+
+    for channel in range(channels_cnt):
+        pos = 0
+        noise = np.fft.fft(data[:, channel][:window_size])
+        while pos + window_size < data_size:
+            transformed_audio = np.fft.fft(data[:, channel][pos: pos + window_size]) - noise
+            data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
+            pos += window_size
+
+    scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
+
+
+bandpass_filter("audio_files/interviews/sample_11.mp3")
