4f65728d9f0e8f11261ae30dc88e366965e43431
John Doe <johndoe@example.com>
2023-05-24 09:15:03 +0300

    structure updates

---
+++ filters.py
@@ -0,0 +1,165 @@
+import os
+from pydub import AudioSegment
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+class AudioFile:
+    def __init__(self, arg1, arg2):
+        """
+        initializer of AudioFile. 2 different types of arguments:
+            1) arg1 - input file name, arg2 - output file name
+            2) arg1 - rate, arg2 - data (check output of scipy.io.wavfile.read)
+        """
+        if type(arg1) == str:
+            if '.mp3' not in arg1 and '.wav' not in arg1:
+                print("Error: Invalid input file format, use mp3 or wav file")
+                sys.exit(0)
+            if '.mp3' not in arg2 and '.wav' not in arg2:
+                print("Error: Invalid output file format, use mp3 or wav file")
+                sys.exit(0)
+
+            self.input_file_name = arg1
+            self.output_file_name = arg2
+            if '.mp3' in arg1:
+                dst = arg1.replace(".mp3", ".wav")
+                try:
+                    sound = AudioSegment.from_mp3(arg1)
+                    sound.export(dst, format="wav")
+                    self.rate, self.data = scipy.io.wavfile.read(dst)
+                    os.remove(dst)
+                except:
+                    print("Error: Unable to read input file")
+                    sys.exit(0)
+            else:
+                self.rate, self.data = scipy.io.wavfile.read(arg1)
+        else:
+            self.rate = arg1
+            self.data = arg2
+
+        self.channels_cnt = 1
+        try:
+            self.channels_cnt = self.data.shape[1]
+        except:
+            pass
+
+    def save(self):
+        """
+        saves file to self.output_file_name
+        """
+        try:
+            if ".wav" in self.output_file_name:
+                scipy.io.wavfile.write(self.output_file_name, rate=self.rate, data=self.data)
+            else:
+                dst = self.output_file_name
+                dst.replace("mp3", "wav")
+                scipy.io.wavfile.write(dst, rate=self.rate, data=self.data)
+                AudioSegment.from_wav(dst).export(self.output_file_name, format="mp3")
+                os.remove(dst)
+        except:
+            print("Error: Unable to save output file")
+            sys.exit(0)
+
+    def bandpass_filter(self):
+        """
+        function that reduces high and low frequency noise
+        """
+        window_size = self.rate
+        data_size = len(self.data)
+
+        for channel in range(self.channels_cnt):
+            pos = 0
+            while pos + window_size < data_size:
+                transformed_audio = np.fft.fft(self.data[:, channel][pos: pos + window_size])
+                size = transformed_audio.size
+                transformed_audio[:200] = 0
+                transformed_audio[-200:] = 0
+                transformed_audio[5000:size // 2] = 0
+                transformed_audio[size // 2:-5000] = 0
+                self.data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
+                pos += window_size
+
+    def exponentional_filter(self):
+        """
+        function that cleans the spectrum using a exponential filter.
+        as a result, high and low frequencies are seriously cleared,
+        the rest of the frequencies are smoothed out
+        """
+        window_size = int(self.rate * 1.5)
+        data_size = len(self.data)
+
+        for channel in range(self.channels_cnt):
+            pos = 0
+            while pos + window_size < data_size:
+                n = window_size
+                if pos + 2 * window_size > data_size:
+                    n = data_size - pos
+
+                transformed_audio = np.fft.fft(self.data[:, channel][pos: pos + n])
+                inds = np.arange(n)
+                alpha = 0.0000001
+                exp_filter = np.exp(-1.5 * alpha * (np.minimum(inds, n - inds) ** 2) / 2)
+                transformed_audio *= exp_filter
+
+                self.data[:, channel][pos: pos + n] = np.fft.ifft(transformed_audio).real
+                pos += window_size
+
+    def spectral_subtraction_filter(self):
+        """
+        this function finds the quietest 0.1 second and subtracts it from the whole record
+        """
+        window_size = int(self.rate * 0.1)
+        data_size = len(self.data)
+
+        for channel in range(self.channels_cnt):
+            pos = 0
+            noise = np.fft.fft(self.data[:, channel][:window_size])
+            min_level = np.mean(np.abs(noise))
+            while pos + window_size < data_size:
+                transformed_audio = np.fft.fft(self.data[:, channel][pos: pos + window_size])
+                if np.mean(np.abs(transformed_audio)) < min_level:
+                    noise = transformed_audio
+                    min_level = np.mean(np.abs(transformed_audio))
+
+                pos += window_size
+
+            pos = 0
+            while pos + window_size < data_size:
+                transformed_audio = np.fft.fft(self.data[:, channel][pos: pos + window_size]) - noise
+                self.data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
+                pos += window_size
+
+    def improved_spectral_subtraction_filter(self):
+        """
+        this function finds the quietest is an improved version of spectral_subtraction_filter
+        it works pretty much the same, but the cleanup is applied to 0.1 second fragments
+        """
+        window_size = int(self.rate * 0.1)
+        window_size -= window_size % 10
+        data_size = len(self.data)
+
+        for channel in range(self.channels_cnt):
+            external_pos = 0
+            while external_pos + window_size < data_size:
+                internal_audio_data = np.array([self.data[:, channel][external_pos:external_pos + window_size]]).T
+                internal_audio_file = AudioFile(window_size, internal_audio_data)
+                internal_audio_file.spectral_subtraction_filter()
+
+                self.data[external_pos:external_pos + window_size] = internal_audio_file.data
+                external_pos += window_size
+
+    def plotter(self, channel, left, right, title, xlabel, ylabel, is_spec=None):
+        plot_data = self.data[:, channel][left * self.rate:right * self.rate]
+        if is_spec:
+            transformed_audio = np.fft.fft(plot_data)
+            size = transformed_audio.size
+            plot_data = transformed_audio[10:size // 2]
+        plt.plot(plot_data)
+        plt.title(title, fontsize=40)
+        plt.xlabel(xlabel, fontsize=40)
+        plt.xticks(fontsize=25)
+        plt.ylabel(ylabel, fontsize=40)
+        plt.yticks(fontsize=25)
+        plt.show()
--- main.py
+++ main.py
@@ -3,134 +3,47 @@ import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 
+from filters import AudioFile
 
-# функция для целочисленного деления с округлением вверх
-def div_round_up(value, divider):
-    return (value + divider - 1) // divider
+file = AudioFile("audio_files/test_audio/RockAndRoll.wav", "audio_files/test_audio/RockAndRoll_bad.wav")
+file.exponentional_filter()
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Время (в 1 / rate секундах)", ylabel="Амплитуда")
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Частота", ylabel="Амплитуда", is_spec=True)
 
+file = AudioFile("audio_files/test_audio/RockAndRoll.wav", "audio_files/test_audio/RockAndRoll_bad.wav")
+file.spectral_subtraction_filter()
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Время (в 1 / rate секундах)", ylabel="Амплитуда")
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Частота", ylabel="Амплитуда", is_spec=True)
 
-# функция для преобразования mp3 в wav файл, с ним удобнее работать
-def from_mp3_to_wav(src):
-    dst = src.replace(".mp3", ".wav")
-
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
-    return src.replace("mp3", "wav")
-
-
-# функция, определяющая наличие речи
-def contains_speech(src):
-    if ".mp3" in src:
-        src = from_mp3_to_wav(src)
-
-    rate, data = scipy.io.wavfile.read(src)
-    try:
-        if data.shape[1]:
-            data = data.T[0]
-    except:
-        data = data.T
-
-    fourier = np.fft.fft(data)
-    return max(fourier) > 1e7
-
-
-# функция, выполняющая очистку от шума высоких и низких частот
-def bandpass_filter(src):
-    if ".mp3" in src:
-        src = from_mp3_to_wav(src)
-
-    rate, data = scipy.io.wavfile.read(src)
-    channels_cnt = 1
-    window_size = rate
-    data_size = len(data)
-    try:
-        channels_cnt = data.shape[1]
-    except:
-        pass
-
-    for channel in range(channels_cnt):
-        pos = 0
-        while pos + window_size < data_size:
-            transformed_audio = np.fft.fft(data[:, channel][pos: pos + window_size])
-            size = transformed_audio.size
-            transformed_audio[:200] = 0
-            transformed_audio[-200:] = 0
-            transformed_audio[7000:size // 2] = 0
-            transformed_audio[size // 2:-7000] = 0
-            data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
-            pos += window_size
-
-    scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
-
-
-# функция, хорошо очищающая высокочастотный и никочастотный шум
-# также она выполняет сглаживание остального диапазона
-def exponentional_filter(src):
-    if ".mp3" in src:
-        src = from_mp3_to_wav(src)
-
-    rate, data = scipy.io.wavfile.read(src)
-    channels_cnt = 1
-    window_size = int(rate * 1.5)
-    data_size = len(data)
-    try:
-        channels_cnt = data.shape[1]
-    except:
-        pass
-
-    for channel in range(channels_cnt):
-        pos = 0
-        while pos + window_size < data_size:
-            n = window_size
-            if pos + 2 * window_size > data_size:
-                n = data_size - pos
-
-            transformed_audio = np.fft.fft(data[:, channel][pos: pos + n])
-            inds = np.arange(n)
-            alpha = 0.0000001
-            filter = np.exp(-1.5 * alpha * (np.minimum(inds, n - inds) ** 2) / 2)
-            transformed_audio *= filter
-
-            data[:, channel][pos: pos + n] = np.fft.ifft(transformed_audio).real
-            pos += window_size
-
-    scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
-
-
-# функция, очищающая периодический шум
-# по первой 0.1 секунду записи
-def spectral_subtraction_filter(src):
-    if ".mp3" in src:
-        src = from_mp3_to_wav(src)
-
-    rate, data = scipy.io.wavfile.read(src)
-    channels_cnt = 1
-    window_size = int(rate * 0.1)
-    data_size = len(data)
-    try:
-        channels_cnt = data.shape[1]
-    except:
-        pass
-
-    for channel in range(channels_cnt):
-        pos = 0
-        noise = np.fft.fft(data[:, channel][:window_size])
-        max_level = max(noise)
-        while pos + window_size < data_size:
-            transformed_audio = np.fft.fft(data[:, channel][pos: pos + window_size])
-            if max(transformed_audio) < max_level:
-                noise = transformed_audio
-                max_level = max(transformed_audio)
-
-            pos += window_size
-
-        pos = 0
-        while pos + window_size < data_size:
-            transformed_audio = np.fft.fft(data[:, channel][pos: pos + window_size]) - noise
-            data[:, channel][pos: pos + window_size] = np.fft.ifft(transformed_audio).real
-            pos += window_size
-
-    scipy.io.wavfile.write(src[:-4] + "_correct" + ".wav", rate=rate, data=data)
-
-
-spectral_subtraction_filter("audio_files/interviews/sample_11.wav")
+file = AudioFile("audio_files/test_audio/RockAndRoll.wav", "audio_files/test_audio/RockAndRoll_bad.wav")
+file.improved_spectral_subtraction_filter()
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Время (в 1 / rate секундах)", ylabel="Амплитуда")
+file.plotter(channel=0, left=0, right=1, title="", xlabel="Частота", ylabel="Амплитуда", is_spec=True)
+# file.save()
+#
+#
+# import numpy as np
+# import scipy.io.wavfile as wav
+#
+# def add_noise_to_audio(input_file, output_file, noise_level):
+#     # Загрузка аудиофайла
+#     sample_rate, data = wav.read(input_file)
+#
+#     # Генерация шума с тем же размером и форматом, что и аудиофайл
+#     noise = np.random.normal(0, noise_level, data.shape)
+#
+#     # Добавление шума к аудиофайлу
+#     noisy_data = data + noise
+#
+#     # Ограничение значений аудиофайла в диапазоне от -32768 до 32767
+#     noisy_data = np.clip(noisy_data, -32768, 32767)
+#
+#     # Сохранение измененного аудиофайла
+#     wav.write(output_file, sample_rate, noisy_data.astype(np.int16))
+#
+# # Пример использования
+# input_file = "audio_files/test_audio/RockAndRoll.wav"
+# output_file = "audio_files/test_audio/RockAndRoll_bad.wav"
+# noise_level = 400  # Уровень шума (можно изменить по вашему усмотрению)
+#
+# add_noise_to_audio(input_file, output_file, noise_level)
